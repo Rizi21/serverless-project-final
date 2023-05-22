669e401f089ada15b71925ee8505de091d5f5168
Rizi21 <rizwanfarooq14@hotmail.com>
2023-05-22 16:40:53 +0100

    updated cognito code

--- modules/cognito/cognito.tf
+++ modules/cognito/cognito.tf
@@ -1,38 +1,51 @@
-resource "aws_cognito_user_pool" "pool" {
-  name = "mypool"
+
+
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
 }
 
- # Enabling SMS and Software Token Multi-Factor Authentication
-resource "aws_cognito_user_pool" "example" {
-  # ... other configuration ...
+resource "aws_cognito_user_pool" "example_user_pool" {
+  name = "WildRydes"  # Replace with your desired user pool name
 
-  mfa_configuration          = "ON"
-  sms_authentication_message = "Your code is {####}"
+  username_attributes = ["email"]
 
-  sms_configuration {
-    external_id    = "example"
-    sns_caller_arn = aws_iam_role.example.arn
-    sns_region     = "us-east-1"
+  # Password policy configuration
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_uppercase = true
+    require_numbers   = true
+    require_symbols   = true
   }
 
-  software_token_mfa_configuration {
-    enabled = true
-  }
+  # Email configuration
+/*   email_configuration {
+    email_sending_account = "DEVELOPER"
+  } */
 }
 
-# Using Account Recovery Setting
-resource "aws_cognito_user_pool" "test" {
-  name = "mypool"
+resource "aws_cognito_user_pool_client" "example_user_pool_client" {
+  name                   = "WildRydesWebApp"  # Replace with your desired client name
+  user_pool_id           = aws_cognito_user_pool.example_user_pool.id
+/*   generate_secret        = false
+  allowed_oauth_flows    = ["code"]
+  allowed_oauth_scopes   = ["openid", "email"]
+  allowed_oauth_flows_user_pool_client = true */
+}
 
-  account_recovery_setting {
-    recovery_mechanism {
-      name     = "verified_email"
-      priority = 1
-    }
+output "user_pool_id" {
+  value = aws_cognito_user_pool.example_user_pool.id
+}
 
-    recovery_mechanism {
-      name     = "verified_phone_number"
-      priority = 2
-    }
-  }
+output "user_pool_client_id" {
+  value = aws_cognito_user_pool_client.example_user_pool_client.id
+}
+
+resource "aws_ses_email_identity" "example" {
+  email = "rizwan.farooq@and.digital"
+}
+
+resource "aws_ses_domain_mail_from" "example" {
+  domain           = aws_ses_email_identity.example.email
+  mail_from_domain = "rizwan.farooq@and.digital"
 }
