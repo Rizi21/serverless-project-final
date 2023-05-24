4bc90505eebc2d58789fa56a122bb5dbae76805a
Rizi21 <rizwanfarooq14@hotmail.com>
2023-05-24 16:30:37 +0100

    added build settings in root directory

---
+++ modules/amplify/variables.tf
@@ -0,0 +1,4 @@
+variable "build_settings_file" {
+  description = "/Users/rizwanfarooq/serverless-final-project/serverless-project-final/build-settings.json"
+  default     = "build-settings.json"
+}
