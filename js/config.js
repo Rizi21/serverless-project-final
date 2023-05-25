40236924c72c101b08b12aadb121362d1baacb60
Rizi21 <rizwanfarooq14@hotmail.com>
2023-05-25 11:33:22 +0100

    config.js file values added

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_lWP0a8Vof', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'bntap5ppp3ccvhjfn40g6ahj3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2bntap5ppp3ccvhjfn40g6ahj3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
