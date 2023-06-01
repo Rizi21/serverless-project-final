6fe55de6d74954b6b240b2f77c55ecbee97cd179
M-Ryhan-W <93862365+M-Ryhan-W@users.noreply.github.com>
2023-06-01 15:40:10 +0100

    Update config.js

--- wild-ryde-app/js/config.js
+++ wild-ryde-app/js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://srt52c4jce.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://zzufsl7tw0.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
