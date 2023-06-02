322929ef7ad3b3d8590c70c6250b87a7d2a0ae3e
Mohammed Ryhan Wadood <mohammed.ryhan.wadood@and.digital>
2023-06-02 12:19:33 +0100

    working APIGW code

--- modules/api-gw/api-gw.tf
+++ modules/api-gw/api-gw.tf
@@ -231,8 +231,12 @@ resource "aws_api_gateway_integration_response" "options" {
   }
 }
 
-
-
+module "cors" {
+  source = "squidfunk/api-gateway-enable-cors/aws"
+  version = "0.3.3"
+  api_id            = aws_api_gateway_rest_api.wildrydes_api.id
+  api_resource_id   = aws_api_gateway_resource.rideresource.id
+}
 
 
 # resource "aws_api_gateway_method_settings" "ride_method_settings" {
