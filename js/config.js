612712754e6c5c22ec15933173268196de45da33
M-Ryhan-W <93862365+M-Ryhan-W@users.noreply.github.com>
2023-06-05 13:16:32 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_lWP0a8Vof', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2bntap5ppp3ccvhjfn40g6ahj3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_OkkfcFZg1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2tqksj5ip3acbe11vgeo4ge46e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
